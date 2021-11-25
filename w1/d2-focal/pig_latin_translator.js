cd992957baecc1011c3ed21a4aef023d40d5050b
sumaya <sumayaomarr@gmail.com>
2021-11-24 20:52:23 -0500

    day 2

---
+++ w1/d2-focal/pig_latin_translator.js
@@ -0,0 +1,15 @@
+var originalWords = process.argv.slice(2);
+var pigLatinWords = [];
+
+console.log("originalWords is", originalWords);
+console.log("pigLatinWords is", pigLatinWords);
+
+for (var i = 1; i < originalWords.length; i++) {
+  pigLatinWords.push(translateToPigLatin(originalWords[i]));
+}
+
+console.log(pigLatinWords.join(' '));
+
+function translateToPigLatin(word) {
+  return word.slice(2, word.length) + word[0] + "ay";
+}
